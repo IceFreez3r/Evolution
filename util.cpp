ac86c5edd312c18f4dbeb73a7fc6e284bef6c7d4
IceFreez3r <39794212+IceFreez3r@users.noreply.github.com>
2019-12-12 19:15:26 +0100

    Added direction()

--- util.cpp
+++ util.cpp
@@ -4,12 +4,16 @@
 
 using namespace std;
 
-unsigned int distance(const pair<unsigned int, unsigned int> pos, const pair<unsigned int, unsigned int> pos2){
+uint16_t distance(const pair<uint16_t, uint16_t> pos, const pair<uint16_t, uint16_t> pos2){
   return sqrt(pow(pos.first - pos2.first, 2) + pow(pos.second - pos2.second, 2));
 }
 
-pair<unsigned int, unsigned int> move(const pair<unsigned int, unsigned int> pos, const unsigned int direction, const unsigned int step_size){
-  unsigned int x_new = cos(((float)direction)/360) * step_size + pos.first;
-  unsigned int y_new = sin(((float)direction)/360) * step_size + pos.second;
+uint16_t direction(const pair<uint16_t, uint16_t> start_pos, pair<uint16_t, uint16_t> target_pos){
+  return atan2((float)(target_pos.first - start_pos.first) / (float)(target_pos.second - start_pos.second)) / M_PI * 360;
+}
+
+pair<uint16_t, uint16_t> move(const pair<uint16_t, uint16_t> pos, const uint16_t direction, const uint16_t step_size){
+  uint16_t x_new = cos(((float)direction)/360 * M_PI) * step_size + pos.first;
+  uint16_t y_new = sin(((float)direction)/360 * M_PI) * step_size + pos.second;
   return make_pair(x_new, y_new);
 }
--- util.hpp
+++ util.hpp
@@ -1,5 +1,7 @@
 #include <utility>
 
-unsigned int distance(const std::pair<unsigned int, unsigned int> pos, const std::pair<unsigned int, unsigned int> pos2);
+uint16_t distance(const std::pair<uint16_t, uint16_t> pos, const std::pair<uint16_t, uint16_t> pos2);
 
-std::pair<unsigned int, unsigned int> move(const std::pair<unsigned int, unsigned int> pos, const unsigned int direction, const unsigned int step_size);
+uint16_t direction(const pair<uint16_t, uint16_t> start_pos, pair<uint16_t, uint16_t> target_pos)
+
+std::pair<uint16_t, uint16_t> move(const std::pair<uint16_t, uint16_t> pos, const uint16_t direction, const uint16_t step_size);
