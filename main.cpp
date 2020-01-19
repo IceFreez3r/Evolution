b982f5316d7f78b9d95ed97128ca31d024904cb4
nragecho <42480667+nragecho@users.noreply.github.com>
2020-01-19 17:33:15 +0100

    less zero's

--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
-
+// g++ -Wall -Werror -W -pedantic main.cpp environment.cpp dot.cpp util.cpp -o a
 #include "environment.hpp"
 #include "util.hpp"
 #include <iostream>
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+int main() {
   Dot d(1000, 10000, 10, 20, 100, make_pair(0,0));
-  Environment e(100, 10, 1, 10, d);
-  for (int i = 0; i < 10; ++i) {
-    e.tick(10);
+  Environment e(1000, 10, 1, 100, d);
+  for (int i = 0; i < 1000; ++i) {
+    e.tick(100);
     e.printProperties();
     // e.printTestground();
   }
